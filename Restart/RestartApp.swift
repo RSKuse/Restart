9e6872db3c09c51decb1d81948497cb98893260d
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2022-01-31 17:07:08 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Simphiwe Kuse on 2022/01/31.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
